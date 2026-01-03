682aad546ceba7977a297422cd18917e25f7fe9f
BudnikMilton93 <budnikmilton93@gmail.com>
2026-01-03 18:21:29 -0300

    Hook useState

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { HooksApp } from './HooksApp';
+import { TrafficLight } from './01-useState/TrafficLight';
+
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TrafficLight />
   </StrictMode>,
 )
